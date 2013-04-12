7dd7e4d512a68b54c5f88504d13758fadeeddbd2
LukaszK <pabc1122@gmail.com>
2013-04-12 07:14:31 +0200

    Rename assembly, cleanup, add create script for nuget, script for ilmerge dependencies

---
+++ TestDataGenerators/ILMergeDependencies.bat
@@ -0,0 +1,2 @@
+cd bin
+..\..\packages\ilmerge.2.13.0307\ILMerge.exe /out:ExpressiveDataGeneratorsMerged.dll /target:dll /targetplatform:v4 /xmldocs ExpressiveDataGenerators.dll ..\..\packages\Microsoft.TestApi.0.6.0.0\lib\net40\TestApiCore.dll /internalize
--- TestDataGenerators/Properties/AssemblyInfo.cs
+++ TestDataGenerators/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestDataGenerators")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ExpressiveDataGenerators")]
+[assembly: AssemblyDescription("Library generate of fill by data, based no clear specyfication.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestDataGenerators")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Lukasz K.")]
+[assembly: AssemblyProduct("ExpressiveDataGenerators")]
+[assembly: AssemblyCopyright("Lukasz K.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
